dddf753d212f2d62a521c78cd8fd8eaa4c4a6cd0
Suranart Niamcome <suranart.n@scbx.com>
2024-01-26 14:53:15 +0700

    Add generated files from spring boot initializer

---
+++ src/main/java/com/example/restapimongodbtutorial/RestApiMongodbTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.restapimongodbtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiMongodbTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiMongodbTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restapimongodbtutorial/RestApiMongodbTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restapimongodbtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiMongodbTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
